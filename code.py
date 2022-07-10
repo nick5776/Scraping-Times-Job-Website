3c75c09074ac4a5ea4191af58e66321ec6570753
Nikhil Gupta <108759735+nick5776@users.noreply.github.com>
2022-07-10 15:09:38 +0530

    Update code.py

--- code.py
+++ code.py
@@ -18,7 +18,6 @@ for job in jobs:
             print(f"Company Name: {company_name.strip()}")
             print(f"Required Skills: {skills.strip()}")
             print(f"More Info: {more_info}")
-
             print('')
 
 
